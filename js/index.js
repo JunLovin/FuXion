f2c0643d297d732dfcf47e82522da1e161a50f0e
JunLovin <mathiassaid7@outlook.es>
2023-10-05 23:16:43 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -4,3 +4,19 @@ setInterval(function(){
     let random = Math.floor(Math.random() * 2);
     images.src = image[random];
 }, 800);
+
+function readM() {
+    let dots = document.querySelector('.dots');
+    let moreT = document.querySelector('.moreT');
+    let moreTButton = document.querySelector('#readMBtn');
+
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        moreTButton.innerHTML = "Ver más";
+        moreT.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        moreTButton.innerHTML = "Ver menos";
+        moreT.style.display = "inline-block";
+    }
+}
